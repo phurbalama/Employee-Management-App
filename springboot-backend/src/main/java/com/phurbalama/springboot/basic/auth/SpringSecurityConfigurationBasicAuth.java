62422d4aec1ac6ceba6be04813148d597cf99fef
Phurba Lama <phurbalama2002@gmail.com>
2022-02-03 23:41:24 -0500

    Spring Security test

--- springboot-backend/src/main/java/com/phurbalama/springboot/basic/auth/SpringSecurityConfigurationBasicAuth.java
+++ springboot-backend/src/main/java/com/phurbalama/springboot/basic/auth/SpringSecurityConfigurationBasicAuth.java
@@ -1,32 +1,25 @@
-/*
- * package com.phurbalama.springboot.basic.auth;
- * 
- * import org.springframework.context.annotation.Configuration;
- * import org.springframework.http.HttpMethod;
- * import
- * org.springframework.security.config.annotation.web.builders.HttpSecurity;
- * import org.springframework.security.config.annotation.web.configuration.
- * EnableWebSecurity;
- * import org.springframework.security.config.annotation.web.configuration.
- * WebSecurityConfigurerAdapter;
- * 
- * @Configuration
- * 
- * @EnableWebSecurity
- * public class SpringSecurityConfigurationBasicAuth extends
- * WebSecurityConfigurerAdapter {
- * 
- * @Override
- * protected void configure(HttpSecurity http) throws Exception {
- * http.csrf().disable()
- * .authorizeRequests()
- * //check all the option request
- * .antMatchers(HttpMethod.OPTIONS,"/**")
- * .permitAll()
- * .anyRequest().authenticated()
- * .and()
- * .httpBasic();
- * }
- * 
- * }
- */
+package com.phurbalama.springboot.basic.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfigurationBasicAuth extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable()
+    .authorizeRequests()
+    //check all the option request
+    .antMatchers(HttpMethod.OPTIONS,"/")
+    .permitAll()
+    .anyRequest().authenticated()
+    .and()
+    .httpBasic();
+    }
+
+}
